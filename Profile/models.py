7502f705520cd9172b4d4ee9fd8940314bde3515
Jacob <Jacobsitch.edu@gmail.com>
2021-12-02 18:34:33 -0700

    model.py default name removed

--- Profile/models.py
+++ Profile/models.py
@@ -8,7 +8,7 @@ from course.models import Course
 # Create your models here.
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=255, default='Your name')
+    name = models.CharField(max_length=255, null=False, blank=False)
     image = models.ImageField(default='default.jpg', upload_to='profile_images', null=True, blank=True)
     gpa = models.FloatField(default=4.0)
     rating = models.FloatField(default=5.0, editable=False)
